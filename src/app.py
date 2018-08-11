98732d44ed7ecac5173b393cf374067695c6256d
Yusuke Suzuki <cs16047@s.inf.shizuoka.ac.jp>
2018-08-11 17:15:26 +0900

    adminの追加

--- src/app.py
+++ src/app.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import os
 from flask import Flask, render_template, request
-import json
-import datetime
 
 
 app = Flask(__name__, static_folder='assets')
+
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-
-@app.route("/")
+@app.route("/admin")
 def index():
-    return render_template("index.html")
+    return render_template("admin.html")
+
+@app.route("/regist")
+def regist():
+    return render_template("regist.html")
 
 if __name__ == "__main__":
     app.run(port=4555, debug=True)
