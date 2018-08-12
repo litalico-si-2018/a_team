f8947a10a865cb6d3dc1cc8a061a344b29426a28
shunyooo <syunyo1028@yahoo.co.jp>
2018-08-12 10:47:27 +0900

    玉井さんのとマージ

--- src/assets/js/user.js
+++ src/assets/js/user.js
@@ -1,43 +1,127 @@
+STORY_DATA = [
+    {
+        level: 11,
+        next_comment: "今日はお仕事どうだったわん？"
+    },
+    {
+        level: 11,
+        next_comment: "それは大変だわん！"
+    },
+    {
+        level: 11,
+        next_comment: "上司はどんな感じだわん？"
+    }
+]
 
 class Api{
-    
-// 　　一番最初の質問取得
-//    static getLatestQuestion(user_id, callback){
-//        const data = {
-//            id : 1,
-//            comment : "お仕事どうだったわん",
-//            level : 10,
-//        }
-//        callback(data);
-//    }
-    
     // 質問に答えて、次の質問を取得
     static updateQuestion(q_id, answer, callback){
-        const data = {
-            status: 1,
-            level: 11,
-            next_comment: {
-                id: 2,
-                comment: "それは大変だわん！"
-            }
-        }
+        const data = STORY_DATA[story_id];
+        story_id = story_id >= STORY_DATA.length - 1  ? 0 : story_id + 1;
         callback(data);
     }
 }
 
 var now_q_id = 0
+var story_id = 0
+const ans_set = ["good", "normal", "angly"]
+function setup(){
+    for (ans of ans_set){
+        $(`.${ans}`).on('click', function(e){
+            const ans_str = e.target.classList[1]
+            answer(ans_str);
+        })
+    }
+    init_answer();
+}
+setup();
 
-function answer(e){
-    answer_str = e.target.classList[1]
-    answer_no = ["good", "normal", "angly"].indexOf(answer_str)
-    Api.updateQuestion(now_q_id, answer_no, function(data){
-        now_q_id = data.next_comment.id;
-        // 描画を更新
-        $(".text_area .text").text(data.next_comment.comment)
+function toggleFadeoutIcons(ans_id){
+    // 呼び出す毎に回転してフェードアウト。
+    // 1.2secかかる（animation.cssより）。
+    const items = $('.choice_area .item')
+    for(var i=0,l=items.length;i<l;i++){
+        const cl = items[i].classList
+        if (i == ans_id){
+            cl.toggle("rotaionFadeoutAnim");
+        } else {
+            cl.toggle("fadeoutAnim");
+        }
+    }
+}
+
+function toggleFadeinIcons(){
+    // アイコン出現css 付加/除去
+    const items = $('.choice_area')[0];
+    items.classList.toggle('fadeinAnim');
+}
+
+function removeIcons(ans_id, completion){
+    // アイコンアニメーション付きRemove→出現
+    toggleFadeoutIcons(ans_id);
+    setTimeout(function() {
+        $('.choice_area').hide()
+        if(completion){completion();}
+    }, 1200);
+}
+
+function fadeinIcons(completion){
+    $('.choice_area').show();
+    toggleFadeinIcons();
+    setTimeout(function() {
+        toggleFadeinIcons();
+        if(completion){completion();}
+    }, 600);
+}
+
+function init_answer(){
+    Api.updateQuestion(0, 0, function(data){
+        // わんこが話す
+        speech(data.next_comment)
     })
 }
 
-$(".normal").on('click', function(e){
-    answer(e);
-})
+function answer(ans_str){
+    ans_id = ans_set.indexOf(ans_str)
+    console.log(ans_str);
+    // 思考中
+    speech("ふんふん....");
+    removeIcons(ans_id, function(){
+        // アイコンのリセットが終わった段階で、
+        Api.updateQuestion(now_q_id, ans_id, function(data){
+            // わんこが話す
+            speech(data.next_comment, function(data){
+                // 話終わった段階で、
+                // アイコンフェードイン
+                toggleFadeoutIcons(ans_id);
+                fadeinIcons();
+            });
+        })
+    })
+}
+
+
+function speech(text, completion){
+    // わんこが話す
+    text_sl = '.text_area .text'
+    $(text_sl).text("");
+    $.when(typing(text, text_sl)).done(function() {
+        if(completion){completion();}
+    });
+}
+
+
+function typing(text, selector, speed=50){
+    // タイピングアニメーション
+    var spans = '<span>' + text.split('').join('</span><span>') + '</span>';
+    return $(spans).hide().appendTo(selector).each(function (i) {
+        $(this).delay(speed * i).css({
+            display: 'inline',
+            opacity: 0
+        }).animate({
+            opacity: 1
+        }, speed);
+    });
+}
+
 
