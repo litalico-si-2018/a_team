c88e40082dc6a06a3caba6174911e093ded2623d
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-12 09:22:51 +0900

    Adding heart icon when smile is pushed

--- src/assets/js/questions.js
+++ src/assets/js/questions.js
@@ -1,9 +1,10 @@
-$(".fa .fa-smile").on("click", function(){
+$(".fa-smile").on("click", function(){
   $("#answerHeart").addClass("fa fa-heart");
   $(".fa-heart:before").css("display", "inline-block");
+  console.log("aaaaaa");
 })
 
-$(".fa .fa-tired").on("click", function(){
+$(".fa-tired").on("click", function(){
   $("#answerTint").addClass("fa fa-tint");
   $(".fa-tint:before").css("display", "inline-block");
 })
